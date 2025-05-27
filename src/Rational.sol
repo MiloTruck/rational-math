9408a7361824530e9f7d9c67bf90b3bf5333d226
MiloTruck <milotruckk@gmail.com>
2025-05-27 13:22:12 +0800

    Refactor Rational into a uint256 type

--- src/Rational.sol
+++ src/Rational.sol
@@ -1,75 +1,121 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-struct Rational {
-    uint256 numerator;
-    uint256 denominator;
+// Upper 128 bits is the numerator, lower 128 bits is the denominator
+type Rational is uint256;
+
+using {add as +, sub as -, mul as *, div as /, eq as ==, neq as !=} for Rational global;
+using RationalLib for Rational global;
+
+// ======================================== CONVERSIONS ========================================
+
+library RationalLib {
+    // The zero rational has a denominator of 1, i.e. (0, 1)
+    // Note that the default rational is (0, 0), but we treat all rationals with a numerator of 0 as zero
+    Rational constant ZERO = Rational.wrap(1);
+
+    function fromUint128(uint128 x) internal pure returns (Rational) {
+        return Rational.wrap(uint256(x) << 128 | 1);
+    }
+
+    function toUint128(Rational x) internal pure returns (uint128) {
+        (uint256 numerator, uint256 denominator) = fromRational(x);
+        return numerator == 0 ? 0 : uint128(numerator / denominator);
+    }
 }
 
-library RationalMath {
-    function add(Rational memory x, Rational memory y) internal pure returns (Rational memory) {
-        if (x.numerator == 0) return y;
-        if (y.numerator == 0) return x;
+// ======================================== OPERATIONS ========================================
 
-        // (a / b) + (c / d) = (ad + cb) / bd
-        uint256 numerator = x.numerator * y.denominator + y.numerator * x.denominator;
-        uint256 denominator = x.denominator * y.denominator;
+function add(Rational x, Rational y) pure returns (Rational) {
+    (uint256 xNumerator, uint256 xDenominator) = fromRational(x);
+    (uint256 yNumerator, uint256 yDenominator) = fromRational(y);
 
-        return from(numerator, denominator);
+    if (xNumerator == 0) return y;
+    if (yNumerator == 0) return x;
+
+    // (a / b) + (c / d) = (ad + cb) / bd
+    uint256 numerator = xNumerator * yDenominator + yNumerator * xDenominator;
+    uint256 denominator = xDenominator * yDenominator;
+
+    return toRational(numerator, denominator);
+}
+
+function sub(Rational x, Rational y) pure returns (Rational) {
+    (uint256 xNumerator, uint256 xDenominator) = fromRational(x);
+    (uint256 yNumerator, uint256 yDenominator) = fromRational(y);
+
+    if (yNumerator == 0) return x;
+    require(xNumerator != 0, "Underflow");
+
+    // (a / b) - (c / d) = (ad - cb) / bd
+    // a / b >= c / d implies ad >= cb, so the subtraction will never underflow when x >= y
+    uint256 numerator = xNumerator * yDenominator - yNumerator * xDenominator;
+    uint256 denominator = xDenominator * yDenominator;
+
+    return toRational(numerator, denominator);
+}
+
+function mul(Rational x, Rational y) pure returns (Rational) {
+    (uint256 xNumerator, uint256 xDenominator) = fromRational(x);
+    (uint256 yNumerator, uint256 yDenominator) = fromRational(y);
+
+    if (xNumerator == 0 || yNumerator == 0) return RationalLib.ZERO;
+
+    // (a / b) * (c / d) = ac / bd
+    uint256 numerator = xNumerator * yNumerator;
+    uint256 denominator = xDenominator * yDenominator;
+
+    return toRational(numerator, denominator);
+}
+
+function div(Rational x, Rational y) pure returns (Rational) {
+    (uint256 xNumerator, uint256 xDenominator) = fromRational(x);
+    (uint256 yNumerator, uint256 yDenominator) = fromRational(y);
+
+    if (xNumerator == 0) return RationalLib.ZERO;
+    require(yNumerator != 0, "Division by zero");
+
+    // (a / b) / (c / d) = ad / bc
+    uint256 numerator = xNumerator * yDenominator;
+    uint256 denominator = xDenominator * yNumerator;
+
+    return toRational(numerator, denominator);
+}
+
+function eq(Rational x, Rational y) pure returns (bool) {
+    (uint256 xNumerator,) = fromRational(x);
+    (uint256 yNumerator,) = fromRational(y);
+    if (xNumerator == 0 && yNumerator == 0) return true;
+
+    return Rational.unwrap(x) == Rational.unwrap(y);
+}
+
+function neq(Rational x, Rational y) pure returns (bool) {
+    return !eq(x, y);
+}
+
+// ======================================== HELPERS ========================================
+
+function fromRational(Rational v) pure returns (uint256 numerator, uint256 denominator) {
+    numerator = Rational.unwrap(v) >> 128;
+    denominator = Rational.unwrap(v) & type(uint128).max;
+}
+
+function toRational(uint256 numerator, uint256 denominator) pure returns (Rational) {
+    uint256 d = gcd(numerator, denominator);
+    numerator /= d;
+    denominator /= d;
+
+    require(numerator <= type(uint128).max && denominator <= type(uint128).max, "Overflow");
+
+    return Rational.wrap(numerator << 128 | denominator);
+}
+
+function gcd(uint256 x, uint256 y) pure returns (uint256) {
+    while (y != 0) {
+        uint256 t = y;
+        y = x % y;
+        x = t;
     }
-
-    function sub(Rational memory x, Rational memory y) internal pure returns (Rational memory) {
-        if (y.numerator == 0) return x;
-        require(x.numerator != 0, "Underflow");
-
-        // (a / b) - (c / d) = (ad - cb) / bd
-        // a / b >= c / d implies ad >= cb, so the subtraction will never underflow when x >= y
-        uint256 numerator = x.numerator * y.denominator - y.numerator * x.denominator;
-        uint256 denominator = x.denominator * y.denominator;
-
-        return from(numerator, denominator);
-    }
-
-    function mul(Rational memory x, Rational memory y) internal pure returns (Rational memory) {
-        if (x.numerator == 0 || y.numerator == 0) return Rational(0, 1);
-
-        // (a / b) * (c / d) = ac / bd
-        uint256 numerator = x.numerator * y.numerator;
-        uint256 denominator = x.denominator * y.denominator;
-
-        return from(numerator, denominator);
-    }
-
-    function div(Rational memory x, Rational memory y) internal pure returns (Rational memory) {
-        if (x.numerator == 0) return Rational(0, 1);
-        require(y.numerator != 0, "Division by zero");
-
-        // (a / b) / (c / d) = ad / bc
-        uint256 numerator = x.numerator * y.denominator;
-        uint256 denominator = x.denominator * y.numerator;
-
-        return from(numerator, denominator);
-    }
-
-    function from(uint256 x, uint256 y) internal pure returns (Rational memory) {
-        uint256 d = gcd(x, y);
-        return Rational(x / d, y / d);
-    }
-
-    function fromU256(uint256 x) internal pure returns (Rational memory) {
-        return Rational(x, 1);
-    }
-
-    function toU256(Rational memory x) internal pure returns (uint256) {
-        return x.numerator / x.denominator;
-    }
-
-    function gcd(uint256 x, uint256 y) private pure returns (uint256) {
-        while (y != 0) {
-            uint256 t = y;
-            y = x % y;
-            x = t;
-        }
-        return x;
-    }
+    return x;
 }
--- src/Vault.sol
+++ src/Vault.sol
@@ -2,11 +2,9 @@
 pragma solidity ^0.8.13;
 
 import {ERC20} from "lib/solmate/src/tokens/ERC20.sol";
-import {Rational, RationalMath} from "./Rational.sol";
+import {Rational, RationalLib} from "./Rational.sol";
 
 contract Vault {
-    using RationalMath for Rational;
-
     ERC20 public asset;
 
     mapping(address => Rational) internal shares;
@@ -16,38 +14,35 @@ contract Vault {
         asset = _asset;
     }
 
-    function deposit(uint256 amount) external {
-        Rational memory _shares = _convertToShares(amount);
+    function deposit(uint128 amount) external {
+        Rational _shares = _convertToShares(amount);
 
-        shares[msg.sender] = shares[msg.sender].add(_shares);
-        totalShares = totalShares.add(_shares);
+        shares[msg.sender] = shares[msg.sender] + _shares;
+        totalShares = totalShares + _shares;
 
         asset.transferFrom(msg.sender, address(this), amount);
     }
 
-    function withdraw(uint256 amount) external {
-        Rational memory _shares = _convertToShares(amount);
+    function withdraw(uint128 amount) external {
+        Rational _shares = _convertToShares(amount);
 
-        shares[msg.sender] = shares[msg.sender].sub(_shares);
-        totalShares = totalShares.sub(_shares);
+        shares[msg.sender] = shares[msg.sender] - _shares;
+        totalShares = totalShares - _shares;
 
         asset.transfer(msg.sender, amount);
     }
 
-    function balanceOf(address account) external view returns (uint256) {
-        Rational memory totalAssets = RationalMath.fromU256(asset.balanceOf(address(this)));
-        Rational memory assets = shares[account].div(totalShares).mul(totalAssets);
-
-        return assets.toU256();
+    function balanceOf(address account) external view returns (uint128) {
+        Rational assets = shares[account] / totalShares * _totalAssets();
+        return RationalLib.toUint128(assets);
     }
 
-    function _convertToShares(uint256 amount) internal view returns (Rational memory) {
-        if (totalShares.numerator == 0) return RationalMath.fromU256(amount);
+    function _convertToShares(uint128 amount) internal view returns (Rational) {
+        Rational assets = RationalLib.fromUint128(amount);
+        return totalShares == RationalLib.ZERO ? assets : assets / _totalAssets() * totalShares;
+    }
 
-        Rational memory assets = RationalMath.fromU256(amount);
-        Rational memory totalAssets = RationalMath.fromU256(asset.balanceOf(address(this)));
-        Rational memory _shares = assets.div(totalAssets).mul(totalShares);
-
-        return _shares;
+    function _totalAssets() internal view returns (Rational) {
+        return RationalLib.fromUint128(uint128(asset.balanceOf(address(this))));
     }
 }
